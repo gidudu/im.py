756a48fdcdbd03ba3f59657c34aac61a9d4535fe
jhunter-d <jhunter.dunefsky@gmail.com>
2013-04-09 08:01:08 -0300

    Update im.py
    This update adds multithreading functionality to im.py.  This means that you can now send more than one message in a row.

--- im.py
+++ im.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python
-#An instant messaging program implemented in Python by Jacob Dunefsky.
+#An instant messaging program implemented in Python.
 #Created on Sunday, December 30, 2012
 
+import thread
 import socket
 import sys
-import threading
 
-def server_listen():
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def get_cli_msg():
+	while True:
+		r = s.recv(8192)
+		
+		if r == "\quit":
+			s.close()
+			sys.exit(0)
+			
+		print "\n" + sys.argv[1] + ": " + r + "\nYou: "
+	
+def get_serv_msg():
 	while True:
 		r = c.recv(8192)
 		
@@ -14,63 +26,36 @@ def server_listen():
 			c.close()
 			s.close()
 			sys.exit(0)
-		
-		print con_addr[0], ": " + r
-
-def client_listen():
-	while True:
-		r = s.recv(8192)
-		
-		if r == "\quit":
-			s.close()
-			sys.exit(0)
-		
-		print sys.argv[1], ": " + r
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			
+		print "\n" + con_addr[0] + ": " + r + "\nYou: "
 
 if sys.argv[1] == "-l":
 	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	s.bind(('', 5067))
 	s.listen(5)
 	c, con_addr = s.accept()
+	thread.start_new_thread(get_serv_msg, ())
 	
 	while True:
-		r = c.recv(8192)
-		
-		if r == "\quit":
-			c.close()
-			s.close()
-			sys.exit(0)
-		
-		print con_addr[0], ": " + r
-		i = raw_input("You: ")
-		
+		i = raw_input("You: \n")
+			
 		if i == "\quit":
 			c.send("\quit")
 			c.close()
 			s.close()
 			sys.exit(0)
 		
-		c.send(i)		
-		
+		c.send(i)
 else:
 	s.connect((socket.gethostbyname(sys.argv[1]), 5067))
-	print "Chat initiated with " + sys.argv[1] + "!"
-	
+	thread.start_new_thread(get_cli_msg, ())
+		
 	while True:
-		i = raw_input("You: ")
+		i = raw_input("You: \n")
 		
 		if i == "\quit":
 			s.send("\quit")
 			s.close()
 			sys.exit(0)
-		
+			
 		s.send(i)
-		r = s.recv(8192)
-		
-		if r == "\quit":
-			s.close()
-			sys.exit(0)
-		
-		print sys.argv[1] + ": " + r
